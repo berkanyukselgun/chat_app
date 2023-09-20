7ff001216a9324e918649c1707494be0ec123467
Berkan Yükselgün <berkanyukselgun@gmail.com>
2023-09-20 17:24:33 +0300

    group info

--- lib/pages/group_İnfo.dart
+++ lib/pages/group_İnfo.dart
@@ -1,4 +1,6 @@
+import 'package:chat_app/pages/home_screen.dart';
 import 'package:chat_app/service/database_Service.dart';
+import 'package:chat_app/widgets/widgets.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
@@ -6,12 +8,12 @@ class GroupInfo extends StatefulWidget {
   final String groupId;
   final String groupName;
   final String adminName;
-
   const GroupInfo(
-      {super.key,
-      required this.groupId,
+      {Key? key,
+      required this.adminName,
       required this.groupName,
-      required this.adminName});
+      required this.groupId})
+      : super(key: key);
 
   @override
   State<GroupInfo> createState() => _GroupInfoState();
@@ -35,6 +37,14 @@ class _GroupInfoState extends State<GroupInfo> {
     });
   }
 
+  String getName(String r) {
+    return r.substring(r.indexOf("_") + 1);
+  }
+
+  String getId(String res) {
+    return res.substring(0, res.indexOf("_"));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -42,20 +52,109 @@ class _GroupInfoState extends State<GroupInfo> {
         centerTitle: true,
         elevation: 0,
         backgroundColor: Theme.of(context).primaryColor,
-        title: Text("Group Info"),
-        actions: [IconButton(onPressed: () {}, icon: Icon(Icons.exit_to_app))],
+        title: const Text("Group Info"),
+        actions: [
+          IconButton(onPressed: () {}, icon: const Icon(Icons.exit_to_app))
+        ],
       ),
       body: Container(
-        padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-        child: Column(children: [
-          Container(
-            padding: EdgeInsets.all(20),
-            decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(30),
-                color: Theme.of(context).primaryColor),
-          )
-        ]),
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+        child: Column(
+          children: [
+            Container(
+              padding: const EdgeInsets.all(20),
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(30),
+                  color: Theme.of(context).primaryColor.withOpacity(0.2)),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  CircleAvatar(
+                    radius: 30,
+                    backgroundColor: Theme.of(context).primaryColor,
+                    child: Text(
+                      widget.groupName.substring(0, 1).toUpperCase(),
+                      style: const TextStyle(
+                          fontWeight: FontWeight.w500, color: Colors.white),
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 20,
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Group: ${widget.groupName}",
+                        style: const TextStyle(fontWeight: FontWeight.w500),
+                      ),
+                      const SizedBox(
+                        height: 5,
+                      ),
+                      Text("Admin: ${getName(widget.adminName)}")
+                    ],
+                  )
+                ],
+              ),
+            ),
+            memberList(),
+          ],
+        ),
       ),
+    );
+  }
+
+  memberList() {
+    return StreamBuilder(
+      stream: members,
+      builder: (context, AsyncSnapshot snapshot) {
+        if (snapshot.hasData) {
+          if (snapshot.data['members'] != null) {
+            if (snapshot.data['members'].length != 0) {
+              return ListView.builder(
+                itemCount: snapshot.data['members'].length,
+                shrinkWrap: true,
+                itemBuilder: (context, index) {
+                  return Container(
+                    padding:
+                        const EdgeInsets.symmetric(horizontal: 5, vertical: 10),
+                    child: ListTile(
+                      leading: CircleAvatar(
+                        radius: 30,
+                        backgroundColor: Theme.of(context).primaryColor,
+                        child: Text(
+                          getName(snapshot.data['members'][index])
+                              .substring(0, 1)
+                              .toUpperCase(),
+                          style: const TextStyle(
+                              color: Colors.white,
+                              fontSize: 15,
+                              fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                      title: Text(getName(snapshot.data['members'][index])),
+                      subtitle: Text(getId(snapshot.data['members'][index])),
+                    ),
+                  );
+                },
+              );
+            } else {
+              return const Center(
+                child: Text("NO MEMBERS"),
+              );
+            }
+          } else {
+            return const Center(
+              child: Text("NO MEMBERS"),
+            );
+          }
+        } else {
+          return Center(
+              child: CircularProgressIndicator(
+            color: Theme.of(context).primaryColor,
+          ));
+        }
+      },
     );
   }
 }
