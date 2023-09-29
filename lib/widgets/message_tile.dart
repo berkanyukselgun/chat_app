92b14fbe0438fc747e4a8d39cebfc1c3d439b9ef
Berkan Yükselgün <berkanyukselgun@gmail.com>
2023-09-30 01:14:08 +0300

    message tile / last commit

--- lib/widgets/message_tile.dart
+++ lib/widgets/message_tile.dart
@@ -18,6 +18,51 @@ class MessageTile extends StatefulWidget {
 class _MessageTileState extends State<MessageTile> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      padding: EdgeInsets.only(
+          top: 4,
+          bottom: 4,
+          left: widget.sendByMe ? 0 : 24,
+          right: widget.sendByMe ? 24 : 0),
+      alignment: widget.sendByMe ? Alignment.centerRight : Alignment.centerLeft,
+      child: Container(
+        margin: widget.sendByMe
+            ? EdgeInsets.only(left: 30)
+            : EdgeInsets.only(right: 30),
+        padding: EdgeInsets.only(top: 17, bottom: 17, left: 20, right: 20),
+        decoration: BoxDecoration(
+          borderRadius: widget.sendByMe
+              ? BorderRadius.only(
+                  topLeft: Radius.circular(20),
+                  topRight: Radius.circular(20),
+                  bottomLeft: Radius.circular(20))
+              : BorderRadius.only(
+                  topLeft: Radius.circular(20),
+                  topRight: Radius.circular(20),
+                  bottomRight: Radius.circular(20)),
+          color: widget.sendByMe ? Theme.of(context).primaryColor : Colors.grey,
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              widget.sender.toUpperCase(),
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                  fontSize: 13,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                  letterSpacing: -0.5),
+            ),
+            SizedBox(height: 8),
+            Text(
+              widget.message,
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 16, color: Colors.white),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
